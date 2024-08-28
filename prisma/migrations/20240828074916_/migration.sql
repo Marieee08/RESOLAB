c4034f745d7f87c468109323ef8ff63d7abe08e4
Marieee08 <jmsano@evc.pshs.edu.ph>
2024-08-28 15:57:49 +0800

    setup database
    uhhh idk basic setup and basic table for start yk

---
+++ prisma/migrations/20240828074916_/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "ClientInfo" (
+    "UserID" TEXT NOT NULL PRIMARY KEY,
+    "Name" TEXT NOT NULL,
+    "Email" TEXT NOT NULL,
+    "ContactNum" TEXT NOT NULL,
+    "Address" TEXT NOT NULL
+);
