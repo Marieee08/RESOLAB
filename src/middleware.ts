01b85c03e64cd5dca2ac4cb7278fa90f504f70dd
rawr <66loneliness99@gmail.com>
2024-11-08 19:31:42 +0800

    FIXED.

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,6 +12,25 @@ const isAdminRoute = createRouteMatcher([
     '/dashboard/admin(.*)'
 ]);
 
+export default clerkMiddleware((auth, req) => {
+    if(isProtectedRoute(req)) {
+      const { userId, sessionClaims } = auth();
+      auth().protect();
+      
+  
+      
+      // Then check for admin routes
+      if(isAdminRoute(req)) {
+        // Assuming you store role in public metadata or claims
+  
+ 
+        }
+      
+    }
+  
+    { debug: true }
+  });
+  
 
 
 export const config = {
